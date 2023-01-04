12d410a6af45d13996d168bb092e9dd31365c6cd
amanda-santos <amandasf0200@gmail.com>
2023-01-04 18:28:36 -0300

    chore: ➕ add styled-components and setup global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme["white"]};
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    color: ${({ theme }) => theme["black"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body {
+    font-family: 'Inter', sans-serif;
+    font-size: 1.6rem;
+    font-weight: 400;
+  }
+`;
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,4 @@
+export const defaultTheme = {
+  white: "#ffffff",
+  black: "#000000",
+};
